d1876c5dfce2bab7fcabf386292d2fc0a9a1082a
GuilhermeBortoluz <guilhermechedidbortoluz@gmail.com>
2024-09-23 17:24:58 -0300

    1001

--- C++ URI/1001/1001.cpp
+++ C++ URI/1001/1001.cpp
@@ -1,12 +1,8 @@
 #include <iostream>
 using namespace std;
-int somar(int a, int b){
-    int soma = a+b;
-    return soma;
-}
 int main(){
     int n1,n2;
     cin >> n1;
     cin >> n2;
-    cout << "X = "<< somar(n1,n2) << endl;
+    cout << "X = "<< n1+n2 << endl;
 }
