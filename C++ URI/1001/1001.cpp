af435b49559aa326d620b6249d472f01d5efc666
GuilhermeBortoluz <guilhermechedidbortoluz@gmail.com>
2024-09-23 10:20:21 -0300

    resolução

--- C++ URI/1001/1001.cpp
+++ C++ URI/1001/1001.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 using namespace std;
-int main()
-{
-    int a,b,x;
-    cin >> a;
-    cin >> b;
-    x=a+b;
-    cout << "X = " << x << endl;
+int somar(int a, int b){
+    int soma = a+b;
+    return soma;
+}
+int main(){
+    int n1,n2;
+    cin >> n1;
+    cin >> n2;
+    cout << "X = "<< somar(n1,n2) << endl;
 }
